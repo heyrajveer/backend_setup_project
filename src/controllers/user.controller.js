e375426c7514733f83aa36d58b6a6599b6d5a728
Rajveer Kumar <sharmaraveer8287631167@gamil.com>
2025-05-22 16:32:18 +0530

    update controller.js genrate token not working thats way

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -106,7 +106,7 @@ const loginUser =asynchHandler(async(req,res)=>{
     if(!isPasswordValid){
       throw new ApiError(401,"Invalid user Credentials")
     }
-    const{accessToken,refreshToken} = await genrateAccessAndRefreshTOken(user._id)
+    const{accessToken,refreshToken} = await generateAccessAndRefreshToken(user._id)
      const loggedInUser =await User.findById(user._id).select("-passward -refreshToken")
 
 
